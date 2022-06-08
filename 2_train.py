7fedfdb24cbd152742a0274b54899ce8422a8273
jaco267 <timmyhorng@gmail.com>
2022-06-08 16:26:49 +0800

    plot

--- 2_train.py
+++ 2_train.py
@@ -59,10 +59,70 @@ print(len(test_news_df), np.array(x_test).shape, len(y_test))
 
 
 
-
+###################################################
 from sklearn.naive_bayes import MultinomialNB
 classifier = MultinomialNB()
 classifier.fit(x_train,y_train)
 
+yhat = classifier.predict(x_test)
 res=classifier.score(x_test,y_test)
 print(res)
+count=0
+for i in range(len(yhat)):
+    if(yhat[i]==0 and y_test[i]==5):
+        count+=1
+print(count)
+from sklearn.metrics import confusion_matrix
+conf_mat=confusion_matrix(y_test, yhat, labels=[0,1,2,3,4,5])
+
+print("\n\n\n")
+print(conf_mat)
+
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+plt.rcParams['axes.unicode_minus'] = False
+from sklearn.metrics import classification_report
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    # print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    label_mapping = {'國際0':0,'生活1':1,'政治3':2,'體育3':3,'社會4':4,'財經5':5}
+    label_mapping = {y: x for x, y in label_mapping.items()}  #把 key value 相反過來˙
+    def cat(x):
+        return label_mapping[x]
+    classes = list(map(cat,classes))
+    plt.xticks(tick_marks, classes, rotation=30)
+    plt.yticks(tick_marks, classes)
+
+    fmt = 'd'# '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(conf_mat, classes=[0,1,2,3,4,5],normalize= False,  title='Confusion matrix')
+plt.show()
--- practice.py
+++ practice.py
@@ -8,3 +8,14 @@ print(cv.get_feature_names_out())
 print(cv_fit.toarray())
 print(cv_fit.toarray().sum(axis=0))
 
+
+
+
+label_mapping = {'國際':0,'生活':1,'政治':2,'體育':3,'社會':4,'財經':5}
+label_mapping = {y: x for x, y in label_mapping.items()}
+print(label_mapping)
+category=[0,1,2]
+def cat(x):
+    return label_mapping[x]
+num = map(cat,category)
+print(list(num))
